00a1954816cdd30d28f1a801d0c16fe7b440de0f
Rob Thompson <robster970@gmail.com>
2018-03-17 12:25:01 +0000

    Initial commit for webhook listener and docker image mgmt

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $SIERRA_ENV
 docker pull robster970/sierra-nginx:latest
 CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' |awk '{print $1}')
 docker stop $CONTAINER_ID
