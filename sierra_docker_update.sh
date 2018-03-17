c13171f811896d6815c81065919ef2f71b0add2d
Rob Thompson <robster970@gmail.com>
2018-03-17 14:25:40 +0000

    Updated webhook to match against the string from the post request (docker hub does now send JSON) and validate against key attributes.

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -2,6 +2,7 @@
 
 docker pull robster970/sierra-nginx:latest
 CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' |awk '{print $1}')
+echo "Stopping container id: " $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
 docker run -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest
