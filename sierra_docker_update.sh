dce36b96860a919a325911aa6b537c0c75f89c84
Rob Thompson <robster970@gmail.com>
2018-03-17 15:25:54 +0000

    Update to handle null being passed in identifying running container.

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CONTAINER_NAME="sierra-nginx"
+
 docker pull robster970/sierra-nginx:latest
 CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' |awk '{print $1}')
 echo "Stopping container id: " $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
-docker run -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest
+docker run  -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest --name $CONTAINER_NAME
 docker ps -a
