dafd7137ca66d047dd80649d394d07f15903ef00
Rob Thompson <robster970@gmail.com>
2018-03-17 16:44:47 +0000

    Another minor mod

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -2,8 +2,8 @@
 
 CONTAINER_NAME="sierra-nginx"
 
-docker pull robster970/sierra-nginx:latest
 CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' | awk '{print $1}')
+docker pull robster970/sierra-nginx:latest
 echo "Stopping container id: " $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
