648ec05155048f61579fb2f888c6833d54a8759f
Rob Thompson <robster970@gmail.com>
2018-03-17 15:47:46 +0000

    Reverted change.

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -7,5 +7,5 @@ CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-nginx' |awk '{print $1
 echo "Stopping container id: " $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
-docker run  -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest --name $CONTAINER_NAME
+docker run  -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest
 docker ps -a
