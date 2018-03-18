1038537a1abfe178ff0425b49626128b16583f14
Rob Thompson <robster970@gmail.com>
2018-03-18 14:58:00 +0000

    Added date and time stamp so it gets logged in production webhook.log

--- sierra_docker_update.sh
+++ sierra_docker_update.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+current_date_time="`date +%Y-%m-%d:%H-%M-%S`";
+echo $current_date_time;
+
 CONTAINER_ID=$(docker ps -a | grep -v Exit | grep 'sierra-trading' | awk '{print $1}')
+
 docker pull robster970/sierra-nginx:latest
+
 echo "Stopping container id: " $CONTAINER_ID
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
+
 docker run --name sierra-trading -p 80:5000 -d --restart=always -t robster970/sierra-nginx:latest
 docker ps -a
+
 docker system prune -f
 df -k
